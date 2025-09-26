2ae14fff95af74e3f21b924faec23e79e21a9937
Andramb1 <andramaulanabuana09@gmail.com>
2025-09-26 16:29:24 +0700

    Add files via upload

---
+++ Pertemuan 2/Tugas1.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+	int a,b,c,d,e,f;
+	cout <<"Nomor 1 - Penjumlahan";
+	cout <<"\nMasukan 2 Angka";
+	cout <<"\nA = ";
+	cin >> a;
+	cout <<"B = ";
+	cin >> b;
+	cout << "Hasil A + B = " << a+b << endl;
+
+
+	cout <<"Nomor 2 - Pengurangan";
+	cout <<"\nMasukan 2 Angka";
+	cout <<"\nC = ";
+	cin >> c;
+	cout <<"D = ";
+	cin >> d;
+	cout << "Hasil C - D = " << c-d << endl;
+	
+	cout <<"Nomor 3 - Perkalian";
+	cout <<"\nMasukan 2 Angka";
+	cout <<"\nE = ";
+	cin >> e;
+	cout <<"F = ";
+	cin >> f;
+	cout << "Hasil E * F = " << e*f << endl;
+}
